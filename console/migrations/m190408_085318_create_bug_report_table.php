a12368d183f84017ac0d38f7264017e3eefa850c
deadbyback <byback@quartsoft.com>
2019-04-08 14:21:16 +0300

    Добавлена таблица баг-репортов

---
+++ console/migrations/m190408_085318_create_bug_report_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%bug_report}}`.
+ */
+class m190408_085318_create_bug_report_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%bug_report}}', [
+            'bug_id' => $this->primaryKey(),
+            'title' => $this->string(40)->notNull(),
+            'desription' => $this->text(),
+            'playback_steps' => $this->text(),
+            'severity' => $this->string(16)->notNull(),
+            'priority' => $this->string(16)->notNull(),
+            'status' => $this->string(16)->notNull(),
+            'reporter_id' => $this->integer()->notNull(),
+            'destination_id' => $this->integer()->notNull(),
+        ]);
+
+        /*TODO: Реализовать в последующих миграциях ключи комментариев и прикреплённых файлов
+                'comment_id' => $this->integer(),
+                'file_id' => $this->integer(),*/
+
+        $this->createIndex('IDX_reporter_id', '{{%bug_report}}', 'reporter_id');
+        $this->createIndex('IDX_destination_id', '{{%bug_report}}', 'destination_id');
+
+        $this->addForeignKey('FK_reporter_user', '{{%bug_report}}', 'reporter_id',
+            '{{user}}', 'id', 'CASCADE', 'CASCADE');
+        $this->addForeignKey('FK_destination_user', '{{%bug_report}}', 'destination_id',
+            '{{user}}', 'id', 'CASCADE', 'CASCADE');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropForeignKey('FK_destination_user', '{{%bug_report}}');
+        $this->dropForeignKey('FK_reporter_user', '{{%bug_report}}');
+
+        $this->dropIndex('IDX_destination_id', '{{%bug_report}}');
+        $this->dropIndex('IDX_reporter_id', '{{%bug_report}}');
+
+        $this->dropTable('{{%bug_report}}');
+    }
+}
