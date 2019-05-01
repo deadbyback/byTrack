8551f530368663c28ceb359d147ecad09a529a74
deadbyback <byback@quartsoft.com>
2019-05-01 11:32:37 +0300

    Реализован просмотр проектов с пользователем-участником

--- common/models/ProjectSearch.php
+++ common/models/ProjectSearch.php
@@ -2,9 +2,9 @@
 
 namespace common\models;
 
+use Yii;
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
-use common\models\Project;
 
 /**
  * ProjectSearch represents the model behind the search form of `common\models\Project`.
@@ -40,9 +40,11 @@ class ProjectSearch extends Project
      */
     public function search($params)
     {
-        $query = Project::find();
+        $id = Yii::$app->user->id;
 
-        // add conditions that should always apply here
+        $query = Project::find()
+            ->innerJoin('{{project_participants}}', '{{project_participants}}.[[project_id]] = {{project}}.[[id]]' )
+            ->andWhere('{{project_participants}}.[[user_id]] = :id', [':id' => $id]);
 
         $projectDataProvider = new ActiveDataProvider([
             'query' => $query,
@@ -51,18 +53,16 @@ class ProjectSearch extends Project
         $this->load($params);
 
         if (!$this->validate()) {
-            // uncomment the following line if you do not want to return any records when validation fails
-            // $query->where('0=1');
+            $query->where('0=1');
             return $projectDataProvider;
         }
 
-        // grid filtering conditions
         $query->andFilterWhere([
             'id' => $this->id,
         ]);
 
-        $query->andFilterWhere(['like', 'title', $this->title])
-            ->andFilterWhere(['like', 'description', $this->description]);
+        $query->andFilterWhere(['like', 'project.title', $this->title])
+            ->andFilterWhere(['like', 'project.description', $this->description]);
 
         return $projectDataProvider;
     }
