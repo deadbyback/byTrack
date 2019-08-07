f648a1704e110bcb2ff62df9e8968bb8383717f9
byback <byback@quartsoft.com>
2019-08-07 14:53:33 +0300

    [#6]: убраны лишние комментарии

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -35,7 +35,7 @@ AppAsset::register($this);
             'class' => 'navbar-inverse navbar-fixed-top',
         ],
     ]);
-    /*TODO: разобраться, почему не работает маршрутизация на <controller>/<action> - done!*/
+
     if (\Yii::$app->user->can('admin') && !Yii::$app->user->isGuest){
     $menuItems = [
             ['label' => 'Front', 'url' => '/'],
@@ -51,18 +51,7 @@ AppAsset::register($this);
             ],],
     ];
     }
-    /*if (Yii::$app->user->isGuest) {
-        $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
-    } else {
-        $menuItems[] = '<li>'
-            . Html::beginForm(['/site/logout'], 'post')
-            . Html::submitButton(
-                'Logout (' . Yii::$app->user->identity->username . ')',
-                ['class' => 'btn btn-link logout']
-            )
-            . Html::endForm()
-            . '</li>';
-    }*/
+
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => $menuItems,
